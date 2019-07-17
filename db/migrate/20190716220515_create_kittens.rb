bde903d3b9fe5304f03a21ed46941abd10a81147
offorma <offorma@gmail.com>
2019-07-17 01:29:20 +0100

    generated kittens scaffold

--- db/migrate/20190716220515_create_kittens.rb
+++ db/migrate/20190716220515_create_kittens.rb
@@ -1,9 +1,9 @@
 class CreateKittens < ActiveRecord::Migration[5.2]
   def change
     create_table :kittens do |t|
-      t.string :name,
-      t.string :age,
-      t.string :cuteness,
+      t.string :name
+      t.string :age
+      t.string :cuteness
       t.string :softness
 
       t.timestamps
